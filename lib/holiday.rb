71195364295212f49e61b87757bb3b17951d449c
Jeffrey Marbury <jeff.marbury@gmail.com>
2019-12-21 23:07:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holiday|
     holiday.map do |holiday, item|
-    holiday if item.includes?("BBQ")
+    holiday if item.include?("BBQ")
     end
   end
     end
