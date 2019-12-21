98d2b2ecb973a77b8f95c7fb80d8cc836ce2f8b3
Jeffrey Marbury <jeff.marbury@gmail.com>
2019-12-21 22:51:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,6 +71,6 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |holidays, supplies|
     if supplies === "BBQ"
       puts holiday_hash# return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
+  end# include the string "BBQ"
 
 end
