3ef6ea83dbce9c0bbab43bb93ffd9da87f8a71bb
Jeffrey Marbury <jeff.marbury@gmail.com>
2019-12-21 21:39:30 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -21,7 +21,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  holiday_supplies[:winter].each do |holiday, item|
+  holiday_hash[:winter].each do |holiday, item|
     item << supply
   end
   end#holiday_hash is identical to the one above
